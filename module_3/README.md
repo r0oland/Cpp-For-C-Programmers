f8e6431c6203ed9571c1d12156a7aa263f0e91f2
Johannes Rebling <johannesrebling@gmail.com>
2022-08-01 13:11:50 +0000

    - lists and pointers and other fun

---
+++ module_3/README.md
@@ -0,0 +1,76 @@
+## Reference Abstractions:  
+
+Implementing Dijkstra’s algorithm requires thinking about at least three basic abstractions: 
+- Graph G = (V, E)
+- PriorityQueue
+- ShortestPath algorithm
+
+Additionally, deciding on a scheme for naming vertices (V) is an important first step in implementation design.
+By convention, vertices are generally mapped onto the set of Integers in the range from 0 : |V| -1.  
+This provides an effective Key into sequential containers (like ARRAY) to access vertex records in constant time - Θ(1). 
+
+
+
+### Class Graph:
+
+A potential partial interface definition for a Graph could be:
+
+**methods**
+- V(G): returns the number of vertices in the graph
+- E(G): returns the number of edges in the graph
+- adjacent (G, x, y): tests whether there is an edge from node x to node y.
+- neighbors (G, x): lists all nodes y such that there is an edge from x to y.
+- add (G, x, y): adds to G the edge from x to y, if it is not there.
+- delete (G, x, y): removes the edge from x to y, if it is there.
+- get_node_value (G, x): returns the value associated with the node x.
+- set_node_value( G, x, a): sets the value associated with the node x to a.
+- get_edge_value( G, x, y): returns the value associated to the edge (x,y).
+- set_edge_value (G, x, y, v): sets the value associated to the edge (x,y) to v.
+
+One important consideration for the Graph class is how to represent the graph as a member ADT. 
+Two basic implementations are generally considered: 
+- adjacency list 
+- adjacency matrix 
+depending on the relative edge density. 
+
+For sparse graphs, the list approach is typically more efficient, but for dense graphs, the matrix approach can be more efficient (reference an Algorithm’s source for space and time analysis). Note in some cases such as add(G, x, y) you may also want to have the edge carry along its cost. 
+Another approach could be to use (x, y) to index a cost stored in an associated array or map.
+
+### Class PriorityQueue
+
+The value of the PriorityQueue is to always have access to the vertex with the next shortest link in the shortest path calculation at the top of the queue. A typically implementation is a minHeap:
+
+- chgPrioirity(PQ, priority): changes the priority (node value) of queue element.
+- minPrioirty(PQ): removes the top element of the queue.
+- contains(PQ, queue_element): does the queue contain queue_element.
+- Insert(PQ, queue_element): insert queue_element into queue
+- top(PQ):returns the top element of the queue.
+- size(PQ): return the number of queue_elements.
+
+### Class ShortestPath
+
+Finally, the class: ShortestPathAlgo - implements the mechanics of Dijkstra’s algorithm. Besides having member fields (has a relationship) of Graph and Priority Queue, an additional ADT maybe required to maintain the parent relationship of the shortest path.
+
+- vertices(List): list of vertices in G(V,E).
+- path(u, w): find shortest path between u-w and returns the sequence of vertices representing shorest path u-v1-v2-…-vn-w.
+- path_size(u, w): return the path cost associated with the shortest path.
+
+The class implementing your Monte Carlo simulation is the workflow manager for this assignment, but other helper classes may be necessary depending on your particular implementation
+
+## Notes and Reminders
+
+Write an appropriate set of constructors for each of your classes ensuring proper initialization – especially think about the process for declaring and initializing a graph.
+
+In this implementation, assume that an edge will have a positive cost function like distance (no negative edge cost).
+
+Assume the graph edges (E)  are undirected.
+
+Ensure that your ADTs support a graph of at least size 50.
+
+The random graph procedure should have edge density as a parameter and distance range as a parameter.
+
+Random graph generator should generate a sufficient set of edges to satisfy the edge density parameter, and each edge should be assigned a randomly generated cost based on the distance range parameter.
+
+So a graph whose density is 0.1 would have 10% of its edges picked at random and its edge distance would be selected at random from the distance range.
+
+Compute for a set of randomly generated graphs an average shortest path.
---
+++ module_3/main.cpp
@@ -0,0 +1,228 @@
+// based in part on infos found in the following sources
+// https://en.wikipedia.org/wiki/Dijkstra's_algorithm#
+// https://www.youtube.com/watch?v=2E7MmKv0Y24
+
+#include <stdlib.h> /* srand, rand */
+#include <iostream>
+#include <vector>
+
+constexpr int32_t nNodes     = 5;
+constexpr float graphDensity = 0.1f;
+
+using namespace std;
+
+class listElement {
+public:
+  listElement(int n = 0, listElement* ptr = nullptr) : d(n), next(ptr) {};
+  int d;
+  listElement* next;
+
+private:
+};
+
+class list {
+public:
+  list() : head(nullptr), cursor(nullptr) {};
+  ~list() {}; // destructor
+
+  list(const list& lst) {
+    if (lst.head == nullptr) {
+      head   = nullptr;
+      cursor = nullptr;
+    } else {
+      // setup new list element pointing to Null
+      head             = new listElement(lst.head->d);
+      cursor           = head;
+      // create temporary pointer to traverse list
+      listElement* ptr = nullptr;
+      // check if we have more to do...
+      ptr = lst.head->next;
+      while (ptr != nullptr) {
+        cursor->next = new listElement(ptr->d);
+        cursor       = cursor->next;
+        ptr          = ptr->next;
+      }
+      cursor->next = nullptr;
+    }
+  }
+
+  void prepend(int n); // insert at front value n
+  int get_element() {
+    return cursor->d; // get value of cursor
+  }
+  void advance() {
+    cursor = cursor->next; // advance cursor to next element
+  }
+  void print(); // print list
+
+  // overload << for printing list on cout
+  friend ostream& operator<<(ostream& os, const list& l) {
+    listElement* ptr = l.head;
+    while (ptr != nullptr) {
+      os << ptr->d << "->";
+      ptr = ptr->next;
+    }
+    os << "NULL";
+    return os;
+  }
+
+private:
+  listElement* head; // head of the list, i.e. latest element
+  listElement* cursor;
+};
+
+void list::prepend(int n) {
+  if (head == nullptr) {
+    cursor = head = new listElement(n);
+  } else {
+    head = new listElement(n, head);
+  }
+}
+
+void list::print(void) {
+  listElement* ptr = head;
+  while (ptr != nullptr) {
+    cout << ptr->d << " ";
+    ptr = ptr->next;
+  }
+  cout << "###" << endl;
+}
+
+bool** create_graph(int32_t nNodes = 50, float prob = 0.5) {
+  bool** graph;
+  srand(time(NULL)); // seed the random number generator
+  graph = new bool*[nNodes];
+  for (int i = 0; i < nNodes; i++) {
+    graph[i] = new bool[nNodes];
+    for (int j = 0; j < nNodes; j++) {
+      if (i == j) {
+        graph[i][j] = false;
+      } else {
+        // see https://cplusplus.com/reference/cstdlib/rand/
+        graph[i][j] = ((rand() % 101) <= static_cast<int>(prob * 100));
+      }
+    }
+  }
+  return graph;
+}
+
+void print_2d_graph(bool** graph, int32_t nNodes) {
+  for (int i = 0; i < nNodes; i++) {
+    for (int j = 0; j < nNodes; j++) {
+      if (graph[i][j]) {
+        cout << " X ";
+      } else {
+        cout << " _ ";
+      }
+    }
+    cout << endl;
+  }
+}
+
+void print_graph_density(bool** graph, int32_t nNodes) {
+  int32_t nEdges = 0;
+  for (int i = 0; i < nNodes; i++) {
+    for (int j = 0; j < nNodes; j++) {
+      if (graph[i][j]) {
+        nEdges++;
+      }
+    }
+  }
+  int32_t nPossibleEdges = nNodes * (nNodes - 1);
+  float density(static_cast<float>(nEdges) / (nPossibleEdges));
+  cout << "graph density: " << density << "(" << nEdges << "/" << nPossibleEdges
+       << ")" << endl;
+}
+
+bool is_connected(bool** graph, int size) {
+  int o_size = 0, c_size = 0;
+  bool* close = new bool[size];
+  bool* open  = new bool[size];
+  for (int i = 0; i < size; i++)
+    open[i] = close[i] = false;
+  open[0] = true;
+  o_size++;
+  while (o_size > 0) { // while there are more nodes in the open set
+    for (int i = 0; i < size; i++) {
+      if (open[i] && (close[i] == false)) {
+        close[i] = true;
+        c_size++;
+        open[i] = false;
+        o_size--;
+        for (int j = 0; j < size; j++) {
+          if (!close[j]) {
+            if (!open[j] && graph[i][j]) {
+              open[j] = true;
+              o_size++;
+            }
+          }
+        }
+      }
+    }
+  }
+  if (c_size == size)
+    return true;
+  else
+    return false;
+}
+
+class point {
+
+public:
+  point(double x = 0.0, double y = 0.0) : x {x}, y {y} {
+    if (this->x < 0.0 || this->y < 0.0) {
+      throw invalid_argument("x and y must be non-negative");
+    }
+  }
+
+  ~point() {};
+
+  double get_x() const {
+    return x;
+  }
+
+  void set_x(const double newVal) {
+    x = newVal;
+  }
+
+  // overload << for printing point on cout
+  friend ostream& operator<<(ostream& os, const point& p) {
+    os << "(" << p.x << ", " << p.y << ")";
+    return os;
+  }
+
+  const double more               = 0.00001;
+  constexpr static double epsilon = 0.00001;
+
+private:
+  double x;
+  double y;
+};
+
+int main() {
+
+  point myFirstPoint(1.0, 2.0);
+  point mySecondPoint(1.0, 2.0);
+
+  cout << "Welcome to the super fancy stuff" << endl;
+  cout << myFirstPoint << endl;
+  cout << mySecondPoint << endl;
+  cout << myFirstPoint.more << endl;
+
+  bool** newGraph = create_graph(nNodes, graphDensity);
+  print_2d_graph(newGraph, nNodes);
+  print_graph_density(newGraph, nNodes);
+  cout << is_connected(newGraph, nNodes) << endl;
+
+  cout << "==========================================" << endl;
+  list myList;
+  myList.prepend(rand() % 100);
+  myList.prepend(rand() % 100);
+  cout << myList << endl;
+  myList.prepend(rand() % 100);
+  myList.prepend(rand() % 100);
+  cout << myList << endl;
+  myList.prepend(rand() % 100);
+  cout << myList << endl;
+  return 0;
+}
