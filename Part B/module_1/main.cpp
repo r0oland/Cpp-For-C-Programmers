7110dc4fbe40e59776b16da6c0dd9d82e5372d03
Johannes Rebling <johannesrebling@gmail.com>
2022-08-12 13:50:56 +0000

    - shuffling that poker game

--- Part B/module_1/main.cpp
+++ Part B/module_1/main.cpp
@@ -52,14 +52,27 @@ public:
     return v != rhs.v;
   }
 
+  // overload < operator
+  bool operator<(const pips& rhs) const {
+    return v < rhs.v;
+  }
+
+  // overload == operator
+  bool operator==(const pips& rhs) const {
+    return v == rhs.v;
+  }
+
 private:
   int v;
 };
 
+//------------------------------------------------------------------------------
 class card {
 public:
-  card() : s(suit::SPADE), p(1) {}
-  card(suit s, pips p) : s(s), p(p) {}
+  card() : s(suit::SPADE), p(1) {
+  }
+  card(suit s, pips p) : s(s), p(p) {
+  }
 
   friend ostream& operator<<(ostream& os, const card& c);
 
@@ -70,11 +83,13 @@ public:
   pips get_pips() const {
     return p;
   }
+
 private:
   suit s;
   pips p;
 };
 
+//------------------------------------------------------------------------------
 ostream& operator<<(ostream& os, const suit& s) {
   switch (s) {
   case suit::SPADE:
@@ -120,6 +135,20 @@ ostream& operator<<(ostream& os, const card& c) {
   return os;
 }
 
+ostream& operator<<(ostream& os, const vector<card>& v) {
+  for (const auto& c : v) {
+    os << c << endl;
+  }
+  return os;
+}
+
+ostream& operator<<(ostream& os, const vector<pips>& v) {
+  for (const auto& p : v) {
+    os << p << endl;
+  }
+  return os;
+}
+
 // initializes a deck of cards
 void init_deck(vector<card>& deck) {
   for (int iSuit = 0; iSuit < 4; iSuit++) {
@@ -136,57 +165,101 @@ void print_deck(const vector<card>& deck) {
   }
 }
 
-bool is_flush(const vector<card> hand){
+bool is_flush(const vector<card> hand) {
   // make sure hand has at least 5 cards
   if (hand.size() < 5) {
+    cout << "Not enough cards to check for a flush" << endl;
     return false;
+  } else {
+    // cout << "Hand has " << hand.size() << " cards" << endl;
   }
-  // make sure to only check the first 5 cards
-  for (int i = 0; i < 5; i++){
-    if (hand[i].get_suit() != hand[i+1].get_suit()){
+
+  // cout << "Checking for a flush...";
+
+  // make sure to only check the first 5 cards, but we are looking +1 so
+  // we only have to make 4 checks and not 5...
+  for (int i = 0; i < 4; i++) {
+    if (hand[i].get_suit() != hand[i + 1].get_suit()) {
+      // cout << "Not a flush" << endl;
       return false;
     }
   }
+  // cout << "Flush!" << endl;
   return true;
 }
 
-bool is_straight(const vector<card> hand){
+bool is_straight(const vector<card> hand) {
   // make sure hand has at least 5 cards
   if (hand.size() < 5) {
     return false;
   }
 
+  // cout << "Checking for a straigth...";
+
   // extract and sort the first 5 cards
-  vector<pips> pips_hand;
-  for (int i = 0; i < 5; i++){
-    pips_hand.push_back(hand[i].get_pips());
+  vector<pips> pipsHand;
+  for (int i = 0; i < 5; i++) {
+    pipsHand.push_back(hand[i].get_pips());
   }
-  sort(pips_hand.begin(), pips_hand.end());
-  
-  //only check the first 5 cards
-  for (int i = 0; i < 5; i++){
-    pips thisPips = hand[i].get_pips();
-    pips nextPips = hand[i+1].get_pips();
-    if (++thisPips != nextPips){
-      cout << thisPips << " != " << nextPips << endl;
+
+  // sort hand in ascending order
+  sort(pipsHand.begin(), pipsHand.end());
+
+  // handle case where high card is an ace
+  if (pipsHand.back() == 13 && pipsHand[0] == 1) {
+    // move ace from the front to the back
+    std::rotate(pipsHand.begin(),pipsHand.begin()+1,pipsHand.end());
+  }
+
+  // only check the first 5 cards, i.e. check for 4 matches
+  for (int i = 0; i < 4; i++) {
+    pips thisPips = pipsHand[i];
+    pips nextPips = pipsHand[i + 1];
+    if (++thisPips != nextPips) {
+      // cout << "Not a Straight!" << endl;
       return false;
-    } else {
-      cout << thisPips << " == " << nextPips << endl;
     }
   }
+  // cout << "Straight!" << endl;
   return true;
 }
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 int main() {
+  // init random seed
+  srand(time(NULL));
   // create a deck of cards
   vector<card> deck;
   init_deck(deck);
 
-  vector<card> hand;
-  hand.push_back(deck[0]);
+  constexpr int NUM_SHUFFLE = 1000000;
+  
+  int straightCount = 0;
+  int flushCount = 0;
+  int straightFlushCount = 0;
 
-  cout << "Is the deck a flush? " << is_flush(deck) << endl;
-  cout << "Is the deck a flush? " << is_straight(deck) << endl;
+  for (int iShuffle = 0; iShuffle <= NUM_SHUFFLE; iShuffle++) {
+    // shuffle the deck
+    random_shuffle(deck.begin(), deck.end());
+    // pick 5 cards at random and build a hand
+    vector<card> hand;
+    for (int i = 0; i < 5; i++) {
+      hand.push_back(deck[i]);
+    }
+    bool isStraight = is_straight(hand);
+    bool isFlush = is_flush(hand);
+    if (isStraight) {
+      straightCount++;
+    }
+    if (isFlush) {
+      flushCount++;
+    }
+    if (isStraight && isFlush) {
+      straightFlushCount++;
+    }
+  }
+  cout << "Number of times straight: " << straightCount << endl;
+  cout << "Number of times flush: " << flushCount << endl;
+  cout << "Number of times straight flush: " << straightFlushCount << endl;
 }
